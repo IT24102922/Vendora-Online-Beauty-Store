30379536903e4f9de9c11bc9a7de83467d2c35b2
IT24102935 <it24102935@my.sliit.lk>
2026-03-27 12:35:05 +0530

    Initial commit

---
+++ src/main/java/com/group02/vendora/VendoraApplication.java
@@ -0,0 +1,13 @@
+package com.group02.vendora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VendoraApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VendoraApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/group02/vendora/VendoraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group02.vendora;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VendoraApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
