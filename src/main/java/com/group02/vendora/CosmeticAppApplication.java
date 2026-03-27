9bbb89ba1146f0a3397aeabea49126aaf1c68dc4
it24103576 <it24103490@my.sliit.lk>
2026-03-27 14:29:50 +0530

    Backend related dto, service, config, exception, repository, model files.

---
+++ src/main/java/com/group02/vendora/CosmeticAppApplication.java
@@ -0,0 +1,11 @@
+package com.cosmetic.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CosmeticAppApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CosmeticAppApplication.class, args);
+    }
+}
